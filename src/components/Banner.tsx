7ccb8aad5317a7379058ba106f630aced162b2da
Alexander Frize <alexander.frize@gmail.com>
2024-11-14 13:02:00 -0800

    url updates

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,10 +10,9 @@ export function Banner({
   setActiveIndex: (index: number) => void;
 }) {
   return (
-    <section className="flex flex-col bg-contain bg-no-repeat lg:h-full lg:bg-cover mt-[25vh] p-8 lg:mt-[18vh] lg:p-24 bg-[url('assets/1-background.png')] relative">
+    <section className="flex flex-col bg-contain bg-no-repeat lg:h-full lg:bg-cover mt-[25vh] p-8 lg:mt-[18vh] lg:p-24 bg-[url('1-background.png')] relative">
       <div className="absolute inset-0 bg-gradient-top-to-bottom z-20" />
-      <div className="h-[90vw] lg:h-full absolute inset-0 top-[-15vh] right-[15vw] bg-[url('assets/1-foreground-cutout.png')] bg-contain bg-no-repeat bg-right-bottom z-10" />
-      {/* <div className="absolute inset-0 bg-gradient-top-to-bottom" /> */}
+      <div className="h-[90vw] lg:h-full absolute inset-0 top-[-15vh] right-[15vw] bg-[url('1-foreground-cutout.png')] bg-contain bg-no-repeat bg-right-bottom z-10" />
 
       <div className="absolute inset-0 bg-gradient-left-to-right" />
 
